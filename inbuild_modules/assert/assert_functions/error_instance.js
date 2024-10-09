e38c649fe4b1d80804e5f3204deb12c2db4733c6
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-09 12:10:44 +0600

    error_instance assert.throw() in node.js

---
+++ inbuild_modules/assert/assert_functions/error_instance.js
@@ -0,0 +1,18 @@
+
+import assert from "node:assert/strict"
+
+var invalidNum = ()=>{
+  throw new Error("invalid Number")
+};
+
+var func = (a)=>{
+  if (a>10)invalidNum()
+  else console.log("valid")
+}
+
+assert.throws(()=>{
+  func(111)
+});
+
+
+// assert.throws() is used to check that an error is thrown, but it does not print the error because it's designed to catch it for validation.
