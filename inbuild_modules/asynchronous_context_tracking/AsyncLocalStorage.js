b6f302de0811cee5bf16d20b5411d9fa030d9142
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-09 15:41:35 +0600

    more on asyncLocalStorage added

--- inbuild_modules/asynchronous_context_tracking/AsyncLocalStorage.js
+++ inbuild_modules/asynchronous_context_tracking/AsyncLocalStorage.js
@@ -4,9 +4,13 @@ import  {AsyncLocalStorage} from "async_hooks"
 const asyncLocalStorage = new AsyncLocalStorage()
 let requestId = 0
 let requestIdAbout = 0
+let countObj = {
+    requestId:0
+}
 const performTask = async()=>{
     console.log('performing task id root url: ', requestId)
     console.log('performing task id About url:', requestIdAbout)
+    console.log("career :",countObj)
 }
 
 const server = http.createServer(function(req,res){
@@ -21,12 +25,22 @@ const server = http.createServer(function(req,res){
         res.end()
     }else if(req.url === '/about'){
         requestIdAbout++;
+        // run() method is used to create a new asynchronous context and store the data 
         asyncLocalStorage.run(requestIdAbout,async()=>{
             await performTask();
             res.statusCode = 200;
         })
         res.write("this is about page!")
         res.end()
+    
+    }else if(req.url === "/career"){
+        countObj.requestId++;
+        asyncLocalStorage.run(countObj.requestId,async()=>{
+            await performTask()
+            res.statusCode = 200;
+            res.write("This is career page!")
+            res.end()
+        })
     }else{
         res.statusCode = 404;
         res.write("404 Not Found!")
@@ -37,4 +51,4 @@ const server = http.createServer(function(req,res){
 const PORT = 8080;
 server.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}/`)
-})
+});
