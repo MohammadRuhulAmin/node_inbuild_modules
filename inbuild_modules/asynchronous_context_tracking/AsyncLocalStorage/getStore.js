b88b04c0f44172ba4048986f076fdb2ed4531570
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-09 16:57:41 +0600

    getStore with object code

--- inbuild_modules/asynchronous_context_tracking/AsyncLocalStorage/getStore.js
+++ inbuild_modules/asynchronous_context_tracking/AsyncLocalStorage/getStore.js
@@ -1,21 +1,36 @@
-import http from "http"
-import {AsyncLocalStorage} from "async_hooks"
-const asyncLocalStorage = new AsyncLocalStorage();
-const process = ()=>{
-    console.log(`The process request: ${asyncLocalStorage.getStore()}`)
-}
-let reqId = 0
-const server = http.createServer((req,res)=>{
-    if (req.url === "/go"){
-        asyncLocalStorage.run(reqId++,async()=>{
-            process()
-            res.end()
-        })
+import http from 'http';
+import {AsyncLocalStorage} from 'async_hooks';
+
+class Server{
+    constructor(port){
+        this.port = port;
+        this.asyncLocalStorage = new AsyncLocalStorage();
+        this.reqId = 0;
+        this.server = http.createServer(this.requestHandler.bind(this));
     }
-})
+    process (){
+        const store = this.asyncLocalStorage.getStore();
+        console.log(`The porcess request ${store}`)
+    }
+    requestHandler(req, res) {
+        if (req.url === '/go') {
+            this.asyncLocalStorage.run(this.reqId++, async () => {
+                this.process();  // Log the process
+                res.end();       // End the response
+            });
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        }
+    }
+    start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server is running at http://localhost:${this.port}`);
+        });
+    }
+}
+
 
 const port = 8080;
-
-server.listen(port,()=>{
-    console.log(`Server is running at http://localhost:${port}`)
-})
+const serverInstance = new Server(port);
+serverInstance.start();
