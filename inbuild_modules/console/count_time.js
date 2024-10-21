45833a5a18e6245e828c2dd6c1626e9da3f7c05d
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-21 18:03:23 +0600

    count_time of api

---
+++ inbuild_modules/console/count_time.js
@@ -0,0 +1,10 @@
+
+//console.clear()
+console.time('test-time');
+
+// Simulate a time-consuming task
+for (let i = 0; i < 1000000; i++) {
+    
+}
+
+console.timeEnd('test-time');
--- inbuild_modules/console/trace.js
+++ inbuild_modules/console/trace.js
@@ -3,7 +3,14 @@ function firstFunction() {
 }
 
 function secondFunction() {
-    console.trace('Show me');
+    //console.trace('Show me');
 }
 
 firstFunction();
+
+
+console.time('bunch-of-stuff');
+setTimeout(()=>{
+    console.log("Running... ")
+},5000)
+console.timeEnd('bunch-of-stuff');
