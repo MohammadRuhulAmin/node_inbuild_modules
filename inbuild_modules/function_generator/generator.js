023de4f5f5ac31186d80fb9034d08f4d8d2d144f
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-10-22 17:58:26 +0600

    function generator added

---
+++ inbuild_modules/function_generator/generator.js
@@ -0,0 +1,19 @@
+
+function* greeting(name){
+    yield "Hi!"
+    yield "How are you?"
+    yield name
+}
+
+
+const greet = greeting("Ruhul")
+
+console.log(greet.next().value)
+console.log(greet.next().value)
+console.log(greet.next().value)
+
+const greet2 = greeting('Ruhul')
+
+for(let text of greet2){
+    console.log(text)
+}
