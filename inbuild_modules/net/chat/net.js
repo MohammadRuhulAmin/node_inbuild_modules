10b131ef27b0c3fa285e74fa84107fc06cb7dbf1
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-11-02 01:58:17 +0600

    chat system added

---
+++ inbuild_modules/net/chat/net.js
@@ -0,0 +1,50 @@
+import net from "node:net";
+import readline from "node:readline";
+import { stdin as input, stdout as output } from "node:process";
+
+const rl = readline.createInterface({ input, output });
+let socketId = 0;
+const clients = new Map();
+
+function processReceived() {
+    var received = buffered.split('\n');
+    while (received.length > 1) {
+      console.log(received[0]);
+      buffered = received.slice(1).join('\n');
+      received = buffered.split('\n');
+    }
+  }
+let buffered = ''
+const server = net.createServer((socket) => {
+    socketId++;
+    clients.set(socketId, socket);
+    console.log(`server connected with client ${socketId}`);
+    send_msg(socketId);
+
+    socket.on('data', (data) => {
+        buffered += data;
+        processReceived();
+    });
+
+    socket.on('end', () => {
+        console.log(`Client ${socketId} has been disconnected!`);
+    });
+});
+
+function send_msg(clientId) {
+    const clientSocket = clients.get(clientId);
+    clientSocket.write(`Hello client ${clientId}\n`);
+}
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log(`Server is listening on port 3000`);
+});
+
+let inputBuffer = '';
+
+rl.on('line', (line) => {
+    inputBuffer += "called : "+line + '\n';
+    const clientSocket = clients.get(socketId);
+    clientSocket.write(inputBuffer);
+    inputBuffer = ''; // Clear the buffer for the next input
+});
