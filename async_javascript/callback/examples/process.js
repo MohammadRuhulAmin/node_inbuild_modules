72f7675ea907f7dc7e00fc6ab1131678894ac7e9
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-11-02 22:41:54 +0600

    callback examples added

---
+++ async_javascript/callback/examples/process.js
@@ -0,0 +1,20 @@
+
+const process1 = (customer,callback)=>{
+    console.log(`process 1 execution for ${customer}`)
+    callback(customer)
+}
+
+const process2 = (customer,callback)=>{
+    console.log(`process 2 execution for ${customer}`)
+    callback()
+}
+
+const process3 = (customer)=>{
+    console.log(`process 3 execution for ${customer}`)
+}
+
+process1('customer 1',(customer)=>{
+    process2(customer,()=>{
+        process3(customer)
+    })
+})
