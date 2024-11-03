2fd855cf11a3f6929cfe0d6d6790be5d1a167556
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-11-03 12:46:21 +0600

    fileHandler readmode added

--- inbuild_modules/fileSystem/fileHandle.js
+++ inbuild_modules/fileSystem/fileHandle.js
@@ -15,12 +15,51 @@ class manageFile{
         }catch(error){
             console.log(error.message)
         }finally{
-            await fileHandle.close()
+            /**
+             * 
+             * In the case of await fileHandle?.close();:
+
+    If fileHandle is a valid object (for example, if a file was successfully opened), the close() method will be called, and the program will wait for that operation to complete.
+    If fileHandle is null or undefined, the expression will evaluate to undefined, and the close() method will not be called. 
+    This prevents the error that would occur if you attempted to call a method on an undefined variable.
+             * 
+             */
+            await fileHandle?.close()
             console.log(`File handle closed!`)
+        }
+    }
+
+    async readMode(){
+        let fileHandle;
+        try{
+            fileHandle = await fs.open(this.filepath,'r')
+            const stats = await fileHandle.stat()
+            const buffer = new Uint8Array(stats.size)
+            await fileHandle.read({buffer,length:stats.size,position:0})
+            const fileContents = new TextDecoder('utf-8').decode(buffer)
+            console.log(fileContents)
+            console.log(stats)
+        }catch(error){
+            throw new Error('Unable to Read using ReadMode Method')
+        }finally{
+            await fileHandle?.close()
         }
     }
 
 }
 
-const m1 = new manageFile('./tmp/file1')
-m1.appendMode()
+/*
+const object = undefined
+console.log(object?.about)
+*/
+
+try{
+    const m1 = new manageFile('./tmp/file1')
+    m1.appendMode()
+    m1.readMode()
+}catch(err){
+    console.log(err.message)
+}finally{
+    console.log('Program Closed!')
+}
+
