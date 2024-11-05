78f88527726632c08f58e0cd231206510e5bb0d7
MohammadRuhulAmin <ruhulamin.cs.dev@gmail.com>
2024-11-05 12:04:30 +0600

    httpAgent class added

--- inbuild_modules/http/httpClass/httpAgent.js
+++ inbuild_modules/http/httpClass/httpAgent.js
@@ -1,47 +1,34 @@
 import http from "node:http"
-
+const PORT = 3000
 const options = {
     hostname:'httpbin.org',
     path:'/get',
     method:'GET'
 }
-
-
 const server = http.createServer((req,res)=>{
-    if(req.url === '/fetch-data' && req.method === 'GET'){
+    if(req.url === "/fetch-api" && req.method === "GET"){
         http.get(options,(response)=>{
             let data = ''
             response.on('data',(chunk)=>{
                 data += chunk 
             })
             response.on('end',()=>{
+                console.log(data.toString())
                 res.writeHead(200,{'Content-Type':'application/json'})
-                res.end(data.toString())
+                res.end(data)
             })
         }).on('socket',(socket)=>{
             socket.emit('agentRemove')
         }).on('error',(err)=>{
             console.log(err.message)
-            res.end('Error: ' + err.message)
         })
     }else{
-        res.writeHead(404,{'Content-Type':'text/plain'})
+        res.writeHead(404,{'Content-Type':'application/json'})
+        res.write("404 Not Found")
         res.end()
     }
 })
 
-/** LifeCycle of HTTP response */
-http.get(options,(res)=>{
-    let data = ''
-    res.on('data',(chunk)=>{
-        data+= chunk 
-    })
-    res.on('end',()=>{
-        console.log( data.toString())
-    })
-}).on('socket',(socket)=>{
-    socket.emit('agentRemove')
-    console.log('Agent Removed')
-}).on('error',(err)=>{
-    console.log(err.message)
+server.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
 })
